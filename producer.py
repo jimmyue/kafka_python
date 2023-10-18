65b4c746dab7c35539ec1646d49cb088691a5fea
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2023-10-18 11:43:43 +0800

    Update producer.py
    add faker data

--- producer.py
+++ producer.py
@@ -1,7 +1,9 @@
-from kafka import KafkaProducer, KafkaConsumer
-from kafka.errors import kafka_errors
+import random
 import traceback
 import json
+from faker import Faker
+from kafka import KafkaProducer, KafkaConsumer
+from kafka.errors import kafka_errors
 
 #建立消息队列
 def producer_demo(host,topic,value):
@@ -11,15 +13,27 @@ def producer_demo(host,topic,value):
         value_serializer=lambda v: json.dumps(v).encode() )
     # 发送消息
     future = producer.send(topic,value)
-    print("生成数据:",value)
+    print("\nTOPIC:",topic," 生产消息:",value)
     try:
         future.get(timeout=10)  # 监控是否发送成功           
     except kafka_errors:        # 发送失败抛出kafka_errors
         traceback.format_exc()
+    producer.close()
 
 if __name__ == "__main__":
     host=['localhost:9092']
     topic='jimmy_test'
-    for i in range(100):
-        data={"data":"list","num":str(i)}
+    # faker生成测试数据
+    fake = Faker(locale='zh_CN')
+    for i in range(10):
+        data={"name":fake.name(),
+              "gender":random.choice(["男","女"]),
+              "age":fake.random_int(min=18,max=90),
+              "address":fake.address(),
+              "company":fake.company(),
+              "job":fake.job(),
+              "phone":fake.phone_number(),
+              "id_card":fake.ssn(),
+              "email":fake.email()}
+        # 生产消息
         producer_demo(host,topic,data)
