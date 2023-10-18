8ab0e94df78fa7a34b2c8409fcc88d726b6eda5f
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2023-10-18 11:44:38 +0800

    Update consumer.py
    modify memo

--- consumer.py
+++ consumer.py
@@ -1,15 +1,15 @@
-from kafka import KafkaProducer, KafkaConsumer
-from kafka.errors import kafka_errors
 import traceback
 import json
+from kafka import KafkaProducer, KafkaConsumer
+from kafka.errors import kafka_errors
 
 #消费消息
 def consumer_demo(host,topic,groupid=None):
-    consumer = KafkaConsumer(topic,group_id=groupid,bootstrap_servers=host)
+    consumer = KafkaConsumer(topic,group_id=groupid,bootstrap_servers=host,auto_offset_reset='earliest')
     print('消费服务已启动！')
     for message in consumer:
         data=json.loads(message.value.decode())
-        print('消费消息：',data)
+        print("\nTOPIC:",topic,' 消费消息：',data)
 
 if __name__ == "__main__":
     host=['localhost:9092']
