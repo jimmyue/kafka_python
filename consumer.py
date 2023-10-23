b733294aac60d7200b347702a4039f030387354e
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2023-10-23 11:53:04 +0800

    Update consumer.py
    1

--- consumer.py
+++ consumer.py
@@ -5,11 +5,13 @@ from kafka import KafkaConsumer,TopicPartition
 
 # CREATE TABLE `test_jimmy_kafka_data` (
 #   `id` bigint NOT NULL AUTO_INCREMENT,
+#   `datatype` varchar(1) NOT NULL COMMENT '1为生产，2为消费',
 #   `topic` varchar(50) NOT NULL COMMENT '主题',
 #   `partitions` varchar(50) DEFAULT NULL COMMENT '分区',
 #   `offset` varchar(50) DEFAULT NULL COMMENT '偏移量',
 #   `groupid` varchar(50) DEFAULT NULL COMMENT '消费者id',
-#   `req_data` longtext COMMENT '消息数据',
+#   `req_key` varchar(50) DEFAULT NULL COMMENT '消费key',
+#   `req_value` longtext COMMENT '消息value',
 #   `cdate` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '消费时间',
 #   PRIMARY KEY (`id`)
 # ) 
@@ -23,29 +25,31 @@ def consumer_demo(host,topic,groupid=None):
         ,enable_auto_commit=False     #消费完数据后手动commit，一条一条获取
         )
     print('kafka消费服务已启动！')
-    partitions = [TopicPartition(topic, p) for p in consumer.partitions_for_topic(topic)]
-    # total
-    toff = consumer.end_offsets(partitions)
-    toff = [(key.partition, toff[key]) for key in toff.keys()]
-    toff.sort()
-    print("{}所有offset: {}".format(topic,str(toff)))
-    # current
-    coff = [(x.partition, consumer.committed(x)) for x in partitions]
-    coff.sort()
-    print("{}当前offset: {}".format(topic,str(coff)))
-    # cal sum and left
-    toff_sum = sum([x[1] for x in toff])
-    cur_sum = sum([x[1] for x in coff if x[1] is not None])
-    left_sum = toff_sum - cur_sum
-    print("{}剩余未消费: {}".format(topic,left_sum))
+    if consumer.partitions_for_topic(topic):
+        partitions = [TopicPartition(topic, p) for p in consumer.partitions_for_topic(topic)]
+        # total
+        toff = consumer.end_offsets(partitions)
+        toff = [(key.partition, toff[key]) for key in toff.keys()]
+        toff.sort()
+        print("{}所有offset: {}".format(topic,str(toff)))
+        # current
+        coff = [(x.partition, consumer.committed(x)) for x in partitions]
+        coff.sort()
+        print("{}当前offset: {}".format(topic,str(coff)))
+        # cal sum and left
+        toff_sum = sum([x[1] for x in toff])
+        cur_sum = sum([x[1] for x in coff if x[1] is not None])
+        left_sum = toff_sum - cur_sum
+        print("{}剩余未消费: {}".format(topic,left_sum))
     #消费数据，插入数据库
-    db=pymysql.connect(host='10.10.22.172',port=3306,user='lexususer',passwd='Lexus.1234#',db='ways_lexus',charset='utf8')
+    db=pymysql.connect(host='xxxx',port=3306,user='xx',passwd='xx',db='xx',charset='utf8')
     cur = db.cursor()
     for message in consumer:
-        data=json.loads(message.value.decode())
-        result=[topic,str(message.partition),str(message.offset),groupid,str(data)]
-        print ("\n消费消息：%s/%d/%d：value=%s" % (message.topic,message.partition,message.offset,data))
-        sql="insert into test_jimmy_kafka_data(topic,partitions,offset,groupid,req_data) values(%s,%s,%s,%s,%s)"
+        key=json.loads(message.key.decode())
+        value=json.loads(message.value.decode())
+        result=['2',topic,str(message.partition),str(message.offset),groupid,str(key),str(value)]
+        print ("\n消费消息：%s/%d/%d key=%s value=%s" % (message.topic,message.partition,message.offset,key,value))
+        sql="insert into test_jimmy_kafka_data(datatype,topic,partitions,offset,groupid,req_key,req_value) values(%s,%s,%s,%s,%s,%s,%s)"
         cur.execute(sql,result)
         db.commit()
         consumer.commit()
@@ -53,7 +57,7 @@ def consumer_demo(host,topic,groupid=None):
     cur.close()
 
 if __name__ == "__main__":
-    host=['ip1:9092','ip2:9092','ip3:9092']
-    topic='jimmy_test'
-    groupid='jimmy'      
+    host=['IP1:9092','IP2:9092','IP3:9092'] #修改kafka服务地址
+    topic='jimmy_test'                                              #修改需要消费的topic
+    groupid='jimmy'                                                 #修改本次消费的消费者ID
     consumer_demo(host,topic,groupid)
