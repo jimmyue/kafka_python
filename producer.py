537ba2c2f50de6941363cc44d9b2016188d0c9fd
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2023-12-15 14:09:52 +0800

    Update producer.py
    2

--- producer.py
+++ producer.py
@@ -21,31 +21,27 @@ from kafka.errors import kafka_errors
 # ) 
 
 #建立消息队列
-def producer_demo(host,topic,key,value):
+def producer_demo(host,topic,key,value,partition=0):
     producer = KafkaProducer(
         bootstrap_servers=host, 
-        key_serializer=lambda k: json.dumps(k).encode(),
+        key_serializer=lambda k: k.encode(),
         value_serializer=lambda v: json.dumps(v).encode() 
         )
     # 发送消息
-    future = producer.send(topic,key=key,value=value)
+    future = producer.send(topic,key=key,value=value,partition=partition)
     print("\n生产消息： topic=",topic," key=",key," value=",value)
     try:
         record_metadata=future.get(timeout=10)  # 监控是否发送成功           
-    except kafka_errors:        # 发送失败抛出kafka_errors
+    except kafka_errors:                        # 发送失败抛出kafka_errors
         traceback.format_exc()
     producer.close()
     return record_metadata.partition,record_metadata.offset
 
-if __name__ == "__main__":
-    host=['IP1:9092','IP2:9092','IP3:9092'] #修改kafka服务地址
-    topic='jimmy_test'                                              #修改需要生产消费的topic
-    key='updateCustomer'                                            #修改生产消费的key
+def faker_data(host,topic,key,n):
+    fake = Faker(locale='zh_CN')
     db=pymysql.connect(host='XXXX',port=3306,user='XX',passwd='XX',db='XX',charset='utf8')
     cur = db.cursor()
-    # faker生成测试数据
-    fake = Faker(locale='zh_CN')
-    for i in range(5):
+    for i in range(n):
         value={
               "name":fake.name(),
               "gender":random.choice(["男","女"]),
@@ -56,9 +52,10 @@ if __name__ == "__main__":
               "phone":fake.phone_number(),
               "dealer":random.choice(["L0201","L0M33","L0R35","L0X55","L0B17","L0A11","L0U11","L0R30","L0R32","L0X54",None,""]),
               "id_card":fake.ssn(),
-              "email":fake.email()
+              "email":fake.email(),
+              "remark":fake.sentence(),
+              "cdate":fake.date_time_this_month().strftime('%Y/%m/%d %H:%M:%S')
               }
-        # 生产消息
         res=producer_demo(host,topic,key,value)
         result=['1',topic,res[0],res[1],key,str(value)]
         sql="insert into test_jimmy_kafka_data(datatype,topic,partitions,offset,req_key,req_value) values(%s,%s,%s,%s,%s,%s)"
@@ -66,3 +63,10 @@ if __name__ == "__main__":
         db.commit()
     db.close()
     cur.close()
+    
+if __name__ == "__main__":
+    host=['IP1:9092','IP2:9092','IP3:9092']                         #修改kafka服务地址
+    topic='jimmy_test'                                              #修改kafka生产消息的topic
+    key='interface'                                                 #修改kafka生产消息的key
+    n=2                                                             #修改需要制造假数据个数
+    data=faker_data(host,topic,key,n)                               #模拟生成测试数据
